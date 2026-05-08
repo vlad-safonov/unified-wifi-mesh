f35fe0dbf8f6fdcd1249bfb5c58ad360efde713a
Vladislav Safonov <vsafonov@maxlinear.com>
2026-05-08 12:53:44 -0700

    fixed sources

--- src/dm/dm_radio_cap.cpp
+++ src/dm/dm_radio_cap.cpp
@@ -90,7 +90,7 @@ bool dm_radio_cap_t::operator == (const dm_radio_cap_t& obj)
     ret += (memcmp(&this->m_radio_cap_info.ht_cap,&obj.m_radio_cap_info.ht_cap,sizeof(em_ap_ht_cap_t)) != 0);
     ret += (memcmp(&this->m_radio_cap_info.vht_cap,&obj.m_radio_cap_info.vht_cap,sizeof(em_ap_vht_cap_t)) != 0);
     ret += (memcmp(&this->m_radio_cap_info.he_cap,&obj.m_radio_cap_info.he_cap,sizeof(em_ap_he_cap_t)) != 0);
-    ret += !(this->m_radio_cap_info.num_op_classes == obj.m_radio_cap_info.num_op_classes);
+    ret +=       ! (this->m_radio_cap_info.num_op_classes == obj.m_radio_cap_info.num_op_classes);
     ret += (memcmp(&this->m_radio_cap_info.wifi6_cap,&obj.m_radio_cap_info.wifi6_cap,sizeof(em_radio_wifi6_cap_data_t)) != 0);
     ret += (memcmp(&this->m_radio_cap_info.wifi7_cap,&obj.m_radio_cap_info.wifi7_cap,sizeof(em_wifi7_agent_cap_t)) != 0);
     ret += (memcmp(&this->m_radio_cap_info.ch_scan,&obj.m_radio_cap_info.ch_scan,sizeof(em_channel_scan_cap_radio_t)) != 0);
@@ -98,7 +98,7 @@ bool dm_radio_cap_t::operator == (const dm_radio_cap_t& obj)
     ret += (memcmp(&this->m_radio_cap_info.prof_2_ap_cap,&obj.m_radio_cap_info.prof_2_ap_cap,sizeof(em_profile_2_ap_cap_t)) != 0);
     ret += (memcmp(&this->m_radio_cap_info.cac_cap,&obj.m_radio_cap_info.cac_cap,sizeof(em_cac_cap_radio_t)) != 0);
     ret += (memcmp(&this->m_radio_cap_info.metric_interval,&obj.m_radio_cap_info.metric_interval,sizeof(em_metric_cltn_interval_t)) != 0);
-   
+    ret +=       ! (this->m_radio_cap_info.eht_ops.radios_num == obj.m_radio_cap_info.eht_ops.radios_num);
      //em_util_info_print(EM_MGR, "%s:%d: MUH ret=%d\n", __func__, __LINE__,ret);
 
      if (ret > 0)
